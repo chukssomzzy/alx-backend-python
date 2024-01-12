221ee7d950c12bd36b279d3cf46d9b7e61171ec3
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-12 17:53:42 +0000

    none

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Add type annotation to function"""
-from types import NoneType
 from typing import Any, Mapping, TypeVar, Union
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, NoneType] =
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] =
                      None) -> Union[Any, T]:
     """TypeVar annotation"""
     if key in dct:
