e58cfbfeab19be2e1d1fa5df75a03255743d36a9
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-12 07:27:57 +0100

    backward compatibility

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Sum mixed list"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[int | float]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """sum mixture of int and float and return a float"""
     return sum(float(n) for n in mxd_list)
