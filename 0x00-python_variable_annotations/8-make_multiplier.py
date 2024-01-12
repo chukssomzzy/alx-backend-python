f4fd8276657aa53a44a35b106fc83ba2edc106d3
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-12 16:40:56 +0000

    function multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,7 @@
 from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function multipler"""
     def _mul(n: float):
         """Multiply float by multiplier"""
         return n * multiplier
