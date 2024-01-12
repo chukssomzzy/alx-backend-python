4012306d95c7b7465457316862085e33d0c9ff2e
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-12 06:35:25 +0100

    sum of float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Takes a list and return a float"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return sum of float"""
+    return sum(n for n in input_list)
