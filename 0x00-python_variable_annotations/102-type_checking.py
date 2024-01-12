dd2546659b4db08abdaec177ef35d671f54c61d5
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-12 18:55:13 +0000

    no issues

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """type checking with mypy"""
-from typing import List
+from typing import List, Tuple
 
 
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """return a list of int"""
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array: List[int] = [12, 72, 91]
+array: Tuple = (12, 72, 91)
 
 zoom_2x: List[int] = zoom_array(array)
 
