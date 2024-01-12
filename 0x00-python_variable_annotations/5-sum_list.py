ccadf2da706779f7e00256eb548c6945e25d0a0b
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-12 07:19:31 +0100

    expected to inport type list for typing module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Takes a list and return a float"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return sum of float"""
     return sum(n for n in input_list)
