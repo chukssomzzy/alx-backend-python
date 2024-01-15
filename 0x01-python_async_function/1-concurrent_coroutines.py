1407417f64786fef34b4a147fa68d2545b7a37d3
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-15 21:44:38 +0100

    as completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,13 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Run multiple couroutines together"""
-    res = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    """Run multiple couroutines together
+    Args:
+        n (int): defines number of couroutines to spawn
+        max_delay (int): maximum delay of couroutine
+    """
+    res = []
+    for cor in asyncio.as_completed([wait_random(max_delay)
+                                     for _ in range(n)]):
+        res.append(await cor)
     return res
