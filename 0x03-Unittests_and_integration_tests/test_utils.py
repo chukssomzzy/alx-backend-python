ae403332263240e495f303e88537cba0d3ac4fc9
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-04 14:45:01 +0000

    returns mock object with request object

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Parametired a unittest"""
-from typing import Any, Mapping, Sequence
+from typing import Any, Dict, Mapping, Sequence
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
+from requests import Request
 
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,3 +32,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map for exception"""
         with self.assertRaisesRegex(KeyError, error):
             access_nested_map(nested_map=nested_map, path=path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test get_json utils method"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
+        """Test get_json method implementation
+        Args:
+            test_url (str): url to test with
+            payload (dict): return dict from url requests
+        """
+        with patch("utils.requests.get") as mock_get:
+            mock_obj = Mock(autospec=Request)
+            mock_obj.json.return_value = test_payload
+            mock_get.return_value = mock_obj
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_get.assert_called_once_with(test_url)
