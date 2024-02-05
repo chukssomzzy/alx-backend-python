568904ec16fa16d834f9e04dff0a91561df2654c
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-05 09:41:43 +0000

    integration test 🤯

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -110,12 +110,13 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         """Teardown integration test suits"""
         cls.get_patcher.stop()
 
-    def test_public_repos(self):
+    def test_public_repos(self) -> None:
         """Integration test for test_public_repos"""
         client = GithubOrgClient('google')
         self.assertEqual(client.public_repos(), self.expected_repos)
 
-    def test_public_repos_with_license(self):
+    def test_public_repos_with_license(self) -> None:
         """Test public repos with license"""
         client = GithubOrgClient('google')
-        self.assertEqual(client.public_repos(license="apache-2.0"), self.apache2_repos)
+        self.assertEqual(client.public_repos(license="apache-2.0"),
+                         self.apache2_repos)
