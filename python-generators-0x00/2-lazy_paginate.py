23dabbcf4b63184d2d0e0e8efbe0d2d54f16e84a
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2025-05-27 04:09:31 +0000

    feat:  paginate users from db

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Paginate user data from database"""
+
+
+seed = __import__('seed')
+
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(
+        f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+
+def lazy_pagination(page_size):
+    """Lazy paginate user data from the database"""
+    offset = 0
+    while True:
+        users = paginate_users(page_size, offset)
+
+        if not users:
+            break
+        yield users
+        offset += page_size
