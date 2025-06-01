33e91aced48e2bb763bcb9e213c356bd0c3380d2
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2025-06-01 03:06:51 +0000

    feat: asyncio

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""Concurrently execute sql queries"""
+import asyncio
+import aiosqlite
+
+
+async def async_fetch_users():
+    """Fetches all users asynchroneously"""
+    async with aiosqlite.connect("users.db") as db:
+        await db.execute("SELECT * FROM users")
+
+        return await db.execute_fetchall()
+
+
+async def async_fetch_older_user():
+    """Fetch older users"""
+    async with aiosqlite.connect("users.db") as db:
+        await db.execute("SELECT * FROM users WHERE age > 40")
+
+        return await db.execute_fetchall()
+
+
+async def fetch_concurrently():
+    """Fetch users concurrently"""
+    await asyncio.gather(async_fetch_users(), async_fetch_older_user())
+
+
+if __name__ == "__main__":
+    asyncio.run(fetch_concurrently())
